f1a724f0a866ebf77fc8dc7f13961e9a0c7325c7
adrianorezena <adrianocr@gmail.com>
2023-02-28 18:39:34 -0300

    add background image

--- Image Parallax/ContentView.swift
+++ Image Parallax/ContentView.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Image("img_gradient")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
         }
-        .padding()
     }
 }
 
