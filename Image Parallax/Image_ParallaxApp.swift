e67aa0acb3275fef05bb19b0e54116165a0af209
adrianorezena <adrianocr@gmail.com>
2023-02-28 18:33:21 -0300

    Initial Commit

---
+++ Image Parallax/Image_ParallaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_ParallaxApp.swift
+//  Image Parallax
+//
+//  Created by Adriano Rezena on 28/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Image_ParallaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
